81c9f208af27987dc23e41d454234ed17bcbfbee
Charles Angelo Lim <choylim06@gmail.com>
2024-04-12 15:17:03 +0800

    dimensions setup for responsiveness

---
+++ lib/config/provider/dimensions_provider.dart
@@ -0,0 +1,39 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dimensions_provider.freezed.dart';
+part 'dimensions_provider.g.dart';
+
+/// The `DimensionState` class is a freezed union class in Dart that represents different dimensions of
+/// a device, including mobile, tablet, and desktop.
+@freezed
+class DimensionState with _$DimensionState {
+  factory DimensionState.mobile() = DimensionMobile;
+  factory DimensionState.tablet() = DimensionTablet;
+  factory DimensionState.desktop() = DimensionDesktop;
+}
+
+/// The `DimensionController` class is a Riverpod state controller that manages the current dimension
+/// state and provides methods to switch between different dimension states.
+@Riverpod(keepAlive: true)
+class DimensionController extends _$DimensionController {
+  @override
+  DimensionState build() {
+    return DimensionDesktop();
+  }
+
+  /// The function sets the state to DimensionMobile.
+  isMobile() {
+    state = DimensionMobile();
+  }
+
+  /// The function sets the state to indicate that the device is a tablet.
+  isTablet() {
+    state = DimensionTablet();
+  }
+
+  /// The function sets the state to DimensionDesktop.
+  isDesktop() {
+    state = DimensionDesktop();
+  }
+}
---
+++ lib/config/provider/dimensions_provider.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dimensions_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dimensionControllerHash() =>
+    r'6d493d18806eb4143756845d809ed7d48f4d1e9d';
+
+/// The `DimensionController` class is a Riverpod state controller that manages the current dimension
+/// state and provides methods to switch between different dimension states.
+///
+/// Copied from [DimensionController].
+@ProviderFor(DimensionController)
+final dimensionControllerProvider =
+    NotifierProvider<DimensionController, DimensionState>.internal(
+  DimensionController.new,
+  name: r'dimensionControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dimensionControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DimensionController = Notifier<DimensionState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
