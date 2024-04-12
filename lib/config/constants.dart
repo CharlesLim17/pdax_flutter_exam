19e1b8188b23b885480539cb44cc0fcf28fafdd1
Charles Angelo Lim <choylim06@gmail.com>
2024-04-12 15:23:00 +0800

    setup constants

---
+++ lib/config/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Color constants
+const primary = Color(0xFF3BEDAD);
+const background = Color(0xFF171F33);
+
+// Text
+const mainText = Color(0xFFE6E9F0);
+const subText = Color(0xFF717171);
