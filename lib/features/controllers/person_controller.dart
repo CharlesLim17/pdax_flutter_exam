bb6f957deee8e877bededf0ac735b1517f2a69cc
Charles Angelo Lim <choylim06@gmail.com>
2024-04-15 13:13:44 +0800

    update person controller

--- lib/features/controllers/person_controller.dart
+++ lib/features/controllers/person_controller.dart
@@ -2,33 +2,58 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pdax_flutter_exam/config/provider/dio_provider.dart';
 import 'package:pdax_flutter_exam/features/models/person_model.dart';
+import 'package:pdax_flutter_exam/features/providers/person_list_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'person_controller.g.dart';
 
+// =========================================================================
+// Person List Controller
+// =========================================================================
 @riverpod
-class FetchPersons extends _$FetchPersons {
-  // Method to fetch person list
-  FutureOr<PersonModel> getPersons() async {
+class PersonListController extends _$PersonListController {
+  // =========================================================================
+  // Method To Fetch Person Data
+  // =========================================================================
+  Future<void> fetchPersonList() async {
     final dio = ref.watch(dioProvider);
+    final quantity = ref.watch(listQuantityProvider);
+    final isFirstFetch = ref.watch(isPersonFirstFetchProvider);
+
+    debugPrint('Fetch Quantity $quantity');
+    debugPrint('Is First Fetched $isFirstFetch');
 
     try {
       Response response = await dio.get(
         '/persons',
-        queryParameters: {'quantity': 20},
+        queryParameters: {'_quantity': quantity},
       );
 
-      debugPrint('Fetched Persons ${response.data.toString()}');
+      debugPrint('Fetched Persons Status Code ${response.statusCode}');
 
-      return PersonModel.fromJson(response.data);
+      if (response.statusCode == 200) {
+        // Parse response data to list
+        PersonModel personModel = PersonModel.fromJson(response.data);
+        List<Datum> dataList = personModel.data!;
+
+        // Add / Append Values to the list
+        ref.read(personListProvider.notifier).addValues(dataList);
+
+        // After the initial fetch of 10 persons set the quantity for the next fetch to 20
+        if (isFirstFetch) {
+          ref.read(listQuantityProvider.notifier).setValue(20);
+          await ref.read(isPersonFirstFetchProvider.notifier).setValue(false);
+        }
+      }
+
+      //
     } catch (e) {
       debugPrint('Error Fetching Persons: $e');
-      return PersonModel();
     }
   }
 
   @override
-  FutureOr<PersonModel> build() {
-    return getPersons();
+  build() {
+    return fetchPersonList();
   }
 }
--- lib/features/controllers/person_controller.g.dart
+++ lib/features/controllers/person_controller.g.dart
@@ -6,20 +6,22 @@ part of 'person_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$fetchPersonsHash() => r'de1dd5f2bdec87590b7402ace6be8e1d131cb400';
+String _$personListControllerHash() =>
+    r'174fac408b20e2746e4dfc77a92438d6212161cd';
 
-/// See also [FetchPersons].
-@ProviderFor(FetchPersons)
-final fetchPersonsProvider =
-    AutoDisposeAsyncNotifierProvider<FetchPersons, PersonModel>.internal(
-  FetchPersons.new,
-  name: r'fetchPersonsProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$fetchPersonsHash,
+/// See also [PersonListController].
+@ProviderFor(PersonListController)
+final personListControllerProvider =
+    AutoDisposeNotifierProvider<PersonListController, Object?>.internal(
+  PersonListController.new,
+  name: r'personListControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$personListControllerHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$FetchPersons = AutoDisposeAsyncNotifier<PersonModel>;
+typedef _$PersonListController = AutoDisposeNotifier<Object?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
