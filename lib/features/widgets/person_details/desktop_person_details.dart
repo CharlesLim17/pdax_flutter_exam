41197d2e743c45779c7e688e7e140deb0c18a2ce
Charles Angelo Lim <choylim06@gmail.com>
2024-04-17 02:40:04 +0800

    done desktop person details screen

--- lib/features/widgets/person_details/desktop_person_details.dart
+++ lib/features/widgets/person_details/desktop_person_details.dart
@@ -1,15 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pdax_flutter_exam/config/constants.dart' as constants;
+import 'package:pdax_flutter_exam/features/providers/person_details_provider/person_details_provider.dart';
+import 'package:pdax_flutter_exam/features/widgets/person_details/components/desktop_components/desktop_person_details_address.dart';
+import 'package:pdax_flutter_exam/features/widgets/person_details/components/desktop_components/desktop_person_specific_details.dart';
 
 class DesktopPersonDetails extends ConsumerWidget {
   const DesktopPersonDetails({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Text(
-        'Desktop Person Details',
-        style: Theme.of(context).textTheme.headlineLarge,
+    final person = ref.watch(personDetailsProvider);
+
+    //
+    return Scaffold(
+      // =================== Scaffold Styles =================== //
+      backgroundColor: constants.background,
+
+      // =================== Scaffold Body =================== //
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 80.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //
+                const SizedBox(height: 64),
+
+                // =================== Image =================== //
+                Center(
+                  child: SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: ClipOval(
+                      child: Image.network(
+                        person!.image!,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            decoration: const BoxDecoration(
+                              color: constants.mainText,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Empty Image Link',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(
+                                      color: constants.subText,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          );
+                        },
+                        loadingBuilder: (context, child, loadingProgress) {
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              color: constants.primary,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+
+                //
+                const SizedBox(height: 40),
+
+                // =================== Personal Details =================== //
+                DesktopPersonSpecificDetails(person: person),
+
+                //
+                const SizedBox(height: 40),
+
+                //
+                Divider(
+                  color: constants.primary.withOpacity(.30),
+                  thickness: 1.5,
+                ),
+
+                //
+                const SizedBox(height: 40),
+
+                // =================== Address Details =================== //
+                DesktopPersonalDetailsAddress(person: person),
+
+                //
+                const SizedBox(height: 64),
+
+                //
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
